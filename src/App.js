519c683b62c25d61602ab193b09dbf42ad86760a
Adam <adamict.frontend@gmail.com>
2020-06-12 11:02:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {CardList} from "./components/card-list/card-list.component";
+import {SearchBox} from "./components/search-box/search-box.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            monsters: [],
+            searchField: ''
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({searchField: e.target.value});
+    };
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(users => this.setState({monsters: users}))
+    }
+
+    render() {
+        const {monsters, searchField} = this.state;
+        const filteredMonsters = monsters.filter(res =>
+            res.name.toLowerCase().includes(searchField.toLowerCase())
+        );
+        return (
+            <div className="App">
+                <SearchBox
+                    placeholder='Search Monster'
+                    handleChange={this.handleChange}
+                />
+
+                <CardList monsters={filteredMonsters}/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/interfaces/test.interface.ts
@@ -0,0 +1,14 @@
+﻿export interface TestInterface {
+    id:number;
+    name:string;
+    username:string;
+    email:string;
+    phone:string;
+    website:string;
+    company: {
+        name:string;
+        catchPhrase:string;
+        bs:string
+    }
+}
+
